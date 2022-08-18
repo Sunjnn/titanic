888fec044f863921d1d732280c0668c1b3350fef
killTummy <jianansun@foxmail.com>
2022-08-18 18:45:44 +0800

    采用 svm 分类器. 用 GridSearch 寻找最优参数.
    	modified:   .gitignore
    	modified:   main.py
    	modified:   model.py
    	new file:   test.csv
    	new file:   train.csv

--- main.py
+++ main.py
@@ -2,19 +2,30 @@ import pandas as pd
 
 
 from data import read_traintest
-from model import get_RFC
+from model import get_svm as get_model
 
 
 def main():
     train_data, test_data = read_traintest()
+    train_data.Age = train_data.Age.fillna(0)
+    test_data.Age = test_data.Age.fillna(0)
+    test_data.Fare = test_data.Fare.fillna(0)
 
     y = train_data['Survived']
 
-    features = ['Pclass', 'Sex', 'SibSp', 'Parch']
+    features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']
     X = pd.get_dummies(train_data[features])
     X_test = pd.get_dummies(test_data[features])
 
-    model = get_RFC()
+    age_max = max(X.Age.max(), X_test.Age.max())
+    X.Age = X.Age / age_max
+    X_test.Age = X_test.Age / age_max
+
+    fare_max = max(X.Fare.max(), X_test.Fare.max())
+    X.Fare = X.Fare / fare_max
+    X_test.Fare = X_test.Fare / fare_max
+
+    model = get_model()
     model.fit(X, y)
     pred = model.predict(X_test)
 
--- model.py
+++ model.py
@@ -1,8 +1,32 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
 
 
-def get_RFC(n_estimators=100, max_depth=5, random_state=1):
+def get_RFC(n_estimators=150, max_depth=6, random_state=1):
     model = RandomForestClassifier(n_estimators, max_depth=max_depth, random_state=random_state)
+    return model
+
+
+class mySVM():
+    def __init__(self):
+        self.svc = svm.SVC()
+
+    def fit(self, data, target, 
+            parameters={'kernel': ('linear', 'poly', 'rbf', 'sigmoid'), 
+                        'C': list(range(1, 11)), 
+                        'gamma': [x / 200 for x in range(400)],
+                        'degree': list(range(3, 6)),
+                        'coef0': [x / 200 for x in range(400)]}):
+        self.clf = GridSearchCV(self.svc, parameters, n_jobs=-1)
+        self.clf.fit(data, target)
+
+    def predict(self, data):
+        return self.clf.predict(data)
+
+
+def get_svm():
+    model = mySVM()
     return model
 
 
